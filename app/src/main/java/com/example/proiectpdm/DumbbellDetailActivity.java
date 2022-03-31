fd11199a84b5ae08987fc2d81a266be23a18af3d
Rusmir Alexandru <alexandru.rusmir02@e-uvt.ro>
2022-03-31 16:44:11 +0300

    first commit

---
+++ app/src/main/java/com/example/proiectpdm/DumbbellDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.proiectpdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DumbbellDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dumbbell_detail);
+    }
+}
